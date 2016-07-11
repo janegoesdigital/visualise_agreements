269d819509115165109f6e62e85bc24a2852b1c0
Jane Hogan <janeodarling@gmail.com>
2016-07-11 17:07:47 +1000

    Fixed up the file structure.  Worked on clause formatting including partially responsive CSS.

---
+++ iteration_1_pure_html_test/clause_tests/main.js
@@ -0,0 +1,6 @@
+
+
+//clause number needs to be generated.  Need to have main clause number.  To get the clause number we count how many clauses there are from the start.  Could therefore count number of h1s
+// var clause_number =
+
+console.log("Hello Jane and Betty")
