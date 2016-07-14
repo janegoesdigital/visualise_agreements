a99ea8351bfa0b44bc3c4e3e8a2b6e6d0a002a39
Jane Hogan <janeodarling@gmail.com>
2016-07-15 01:36:36 +1000

    Changed from purecss to materialize. All routes working.  Still some content to create.

---
+++ iteration1_agreement_rails/rails_ag1/app/assets/javascripts/main.js
@@ -0,0 +1,15 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+    
+
+  });
+
+  $(document).ready(function(){
+      $('ul.tabs').tabs();
+    });
