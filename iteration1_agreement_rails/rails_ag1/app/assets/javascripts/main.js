2f1d7049ee76510a5365b066f0b6bdbad62b969d
Jane Hogan <janeodarling@gmail.com>
2016-07-15 12:22:01 +1000

    demo version for class. Still problems with the accordion and some routes.

--- iteration1_agreement_rails/rails_ag1/app/assets/javascripts/main.js
+++ iteration1_agreement_rails/rails_ag1/app/assets/javascripts/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     $('.collapsible').collapsible({
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
-    
+
 
   });
 
